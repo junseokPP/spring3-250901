260d9a02dcdcfe20a92fa709c8c420219700e978
junseokPP <pjs981130@gmail.com>
2025-09-02 11:26:12 +0900

    9강 : validation 사용

--- src/main/java/com/back/spring3/domain/post/controller/PostController.java
+++ src/main/java/com/back/spring3/domain/post/controller/PostController.java
@@ -2,14 +2,18 @@ package com.back.spring3.domain.post.controller;
 
 import com.back.spring3.domain.post.entity.Post;
 import com.back.spring3.domain.post.service.PostService;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@Validated
 public class PostController {
 
     private PostService postService;
@@ -53,10 +57,14 @@ public class PostController {
 
     @PostMapping("/posts/doWrite")
     @ResponseBody
-    public String doWrite(String title, String content){
+    public String doWrite(@NotBlank @Size(min =2,max = 10) String title, @NotBlank @Size(min =2,max = 10) String content){
 
-        if(title.isBlank()) return getWriteFormHtml("제목을 입력해주세요.",title,content,"title");
-        if(content.isBlank()) return getWriteFormHtml("내용을 입력해주세요.",title,content,"content");
+//        if(title.isBlank()) return getWriteFormHtml("제목을 입력해주세요.",title,content,"title");
+//        if(title.length() < 2) return getWriteFormHtml("제목은 2글자 이상 적어주세요.",title,content,"title");
+//        if(title.length() > 10) return getWriteFormHtml("제목은 10글자 이상 넘을 수 없습니다.",title,content,"title");
+//        if(content.isBlank()) return getWriteFormHtml("내용을 입력해주세요.",title,content,"content");
+//        if(content.length() < 2) return getWriteFormHtml("내용을 2글자 이상 적어주세요.",title,content,"content");
+//        if(content.length() > 100) return getWriteFormHtml("내용을 100글자 이상 넘을 수 없습니다.",title,content,"content");
 
         Post post = postService.write(title,content);
 
