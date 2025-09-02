4181c7548588d07a680ee336244daa7a671ebf7e
junseokPP <pjs981130@gmail.com>
2025-09-02 15:50:33 +0900

    20강 : 타임리프사용

--- src/main/java/com/back/spring3/domain/post/controller/PostController.java
+++ src/main/java/com/back/spring3/domain/post/controller/PostController.java
@@ -10,6 +10,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
@@ -20,67 +21,64 @@ import java.util.stream.Collectors;
 @Controller
 public class PostController {
 
-    private PostService postService;
+    private final PostService postService;
 
-    @Autowired
     public PostController(PostService postService) {
         this.postService = postService;
-    }
-
-    public PostController() {
     }
 
     @AllArgsConstructor
     @Getter
     public static class PostWriteForm {
         @NotBlank(message = "1-제목을 입력해주세요.")
-        @Size(min = 2, max = 10 ,message = "2-제목은 2글자 이상 10글자 이하로 입력해주세요.")
+        @Size(min = 2, max = 10, message = "2-제목은 2글자 이상 10글자 이하로 입력해주세요.")
         private String title;
 
         @NotBlank(message = "3-내용을 입력해주세요.")
-        @Size(min = 2, max = 100 ,message = "4-내용을 2글자 이상 100글자 이하로입력해주세요.")
+        @Size(min = 2, max = 100, message = "4-내용은 2글자 이상 100글자 이하로 입력해주세요.")
         private String content;
     }
 
 
     @GetMapping("/posts/write")
-    public String write(){
+    public String write() {
         return "post/write";
     }
 
     @PostMapping("/posts/doWrite")
     public String doWrite(
-            @Valid PostWriteForm form, BindingResult bindingResult
-            ){
+            @ModelAttribute("form") @Valid PostWriteForm form, BindingResult bindingResult,
+            Model model
+    ) {
 
-        if(bindingResult.hasErrors()){
-
-            String fieldName = "title";
-
-            FieldError fieldError = bindingResult.getFieldError();
-
+        if(bindingResult.hasErrors()) {
+            // 스트림
             String errorMessages = bindingResult.getFieldErrors()
                     .stream()
-                    .map(field -> field.getField() + " - " + field.getDefaultMessage())
+                    .map(field -> field.getField() + "-" + field.getDefaultMessage())
                     .map(message -> message.split("-"))
-                    .map(bits ->"""
-                            "<!-- %s --><li data-error-field-name="%s">%s</li>"
-                            """.formatted(bits[1], bits[0],bits[2]))
+                    .map(bits -> """
+                            <!-- %s --><li data-error-field-name="%s">%s</li>
+                            """.formatted(bits[1], bits[0], bits[2]))
                     .sorted()
-                    .collect(Collectors.joining("<br>"));
+                    .collect(Collectors.joining("\n"));
+
+            model.addAttribute("errorMessages", errorMessages);
 
             return "post/write";
         }
 
-//        if(title.isBlank()) return getWriteFormHtml("제목을 입력해주세요.",title,content,"title");
-//        if(title.length() < 2) return getWriteFormHtml("제목은 2글자 이상 적어주세요.",title,content,"title");
-//        if(title.length() > 10) return getWriteFormHtml("제목은 10글자 이상 넘을 수 없습니다.",title,content,"title");
-//        if(content.isBlank()) return getWriteFormHtml("내용을 입력해주세요.",title,content,"content");
-//        if(content.length() < 2) return getWriteFormHtml("내용을 2글자 이상 적어주세요.",title,content,"content");
-//        if(content.length() > 100) return getWriteFormHtml("내용을 100글자 이상 넘을 수 없습니다.",title,content,"content");
 
-        Post post = postService.write(form.title,form.content);
+//        if(title.isBlank()) return getWriteFormHtml("제목을 입력해주세요.", title, content, "title");
+//        if(title.length() < 2) return getWriteFormHtml("제목은 2글자 이상 적어주세요.", title, content, "title");
+//        if(title.length() > 10) return getWriteFormHtml("제목은 10글자 이상 넘을 수 없습니다.", title, content, "title");
+//        if(content.isBlank()) return getWriteFormHtml("내용을 입력해주세요.", title, content, "content");
+//        if(content.length() < 2) return getWriteFormHtml("내용은 2글자 이상 적어주세요.", title, content, "content");
+//        if(content.length() > 100) return getWriteFormHtml("내용은 100글자 이상 넘을 수 없습니다.", title, content, "content");
 
+        Post post = postService.write(form.title, form.content);
+
+        model.addAttribute("id", post.getId());
         return "post/writeDone";
     }
 }
