38065e30306fc714785cb8f13a8c7fd3b7a432ed
junseokPP <pjs981130@gmail.com>
2025-09-03 10:33:07 +0900

    22,23강 : th:object,th:field사용

--- src/main/java/com/back/spring3/domain/post/controller/PostController.java
+++ src/main/java/com/back/spring3/domain/post/controller/PostController.java
@@ -41,7 +41,7 @@ public class PostController {
 
 
     @GetMapping("/posts/write")
-    public String write() {
+    public String write(@ModelAttribute("form") PostWriteForm form) {
         return "post/write";
     }
 
